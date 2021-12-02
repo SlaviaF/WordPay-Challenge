6cc6f1b515487744335ca299ceb47277b0168283
SlaviaF <slaviafurtado@gmail.com>
2021-12-02 22:22:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/pages/Navbar';
+import Main from './components/pages/Main';
+//import NavbarB from './components/pages/NavbarB'
+import { BrowserRouter as Router } from "react-router-dom";
+import Tools from './components/pages/Tools';
+import Pricing from './components/pages/Pricing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Navbar />
+        <Main />
+        <Tools />
+        <Pricing />
+      </Router>
     </div>
   );
 }
